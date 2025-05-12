545e8befb8fc0daabc7505c5280dc6986d381a19
reza latib <mrezalatib@gmail.com>
2025-05-13 01:16:58 +0200

    created s3 client and reading from s3 bucket

--- main.py
+++ main.py
@@ -2,3 +2,17 @@ import boto3
 import pandas as pd
 from io import StringIO
 
+
+
+s3 = boto3.client("s3")
+
+bucket_name = "my-spotify-stats-bucket"
+file_name = "spotifydataset.csv"
+
+response = s3.get_object(Bucket=bucket_name, Key=file_name)
+
+csv_content = response['Body'].read().decode('utf-8')
+df = pd.read_csv(StringIO(csv_content))
+
+# Preview the data
+print(df.head())
