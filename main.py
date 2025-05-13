c78b7de072f5208e0fc7db6748887003632c8ae8
reza latib <mrezalatib@gmail.com>
2025-05-13 17:27:16 +0200

    modularized loading csv data from s3

--- main.py
+++ main.py
@@ -3,16 +3,22 @@ import pandas as pd
 from io import StringIO
 
 
-#creates s3 client object (facilitates communication between AWS S3 and python) 
-s3 = boto3.client("s3")
+def load_csv_from_s3(bucket_name: str, file_name: str) -> pd.DataFrame:
+    #creates s3 client object
+    s3 = boto3.client("s3")
+
+    response = s3.get_object(Bucket=bucket_name, Key=file_name)
+    csv_data = response['Body'].read().decode('utf-8') #decode utf-8 makes response body human readable
+
+    #csv_content passed through String_IO because pd.read_csv expects a file or file-like object (not a string)
+    df = pd.read_csv(StringIO(csv_data))
+
+    return df
+
 
 bucket_name = "my-spotify-stats-bucket"
 file_name = "spotifydataset.csv"
 
-response = s3.get_object(Bucket=bucket_name, Key=file_name) #sends request to AWS to retrieve specified csv data
-
-csv_content = response['Body'].read().decode('utf-8') #decode utf-8 makes response body human readable
-df = pd.read_csv(StringIO(csv_content)) #csv_content passed through String_IO because pd.read_csv expects a file or file-like object (not a string)
-
-#preview data from response body on console
-print(df.head())
+df = load_csv_from_s3(bucket_name, file_name)
+ 
+print(df.head()) #a preview of data from response body (its not ALL the data since its too large to display via terminal ofc)
