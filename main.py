b2896c4e6dd6cddbf0ac8b8b1bfb9f25a8e30a5d
reza latib <mrezalatib@gmail.com>
2025-05-14 00:09:44 +0200

    added try except block to catch potential issues with writing df to db

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import boto3
 import pandas as pd
 from io import StringIO
 from sqlalchemy import create_engine
+from sqlalchemy.exc import SQLAlchemyError
 
 
 def load_csv_from_s3(bucket_name: str, file_name: str) -> pd.DataFrame:
@@ -18,12 +19,15 @@ def load_csv_from_s3(bucket_name: str, file_name: str) -> pd.DataFrame:
 
 
 def write_to_sqlite(df, database_name: str, table_name: str):
-    engine = create_engine(f'sqlite:///{database_name}') #create engine which is connection to specified db. sets configs for connection
-    df.to_sql(table_name, con=engine, index=False, if_exists='replace') #create + write datafram to db. index = false because pandas adds index column i dont need that
-
+    try:
+        engine = create_engine(f'sqlite:///{database_name}') #create engine which is connection to specified db. sets configs for connection
+        df.to_sql(table_name, con=engine, index=False, if_exists='replace') #create + write datafram to db. index = false because pandas adds index column i dont need that
+        print(f"Data written to '{table_name}' table in '{database_name}' successfully.")
+    except SQLAlchemyError as e:        
+        print(f"Error: {e}")
 
 bucket_name = "my-spotify-stats-bucket"
 file_name = "spotifydataset.csv"
 
 df = load_csv_from_s3(bucket_name, file_name)
-write_to_sqlite(df, 'spotify_stats.db', 'spotify_stats.db')
+write_to_sqlite(df, 'spotify_stats.db', 'spotify_statistics')
