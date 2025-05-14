5790acb15b4db2f1d1fe26ab8ed2aafa6b646f84
reza latib <mrezalatib@gmail.com>
2025-05-14 17:36:12 +0200

    created helper func that retrieves all songs by certain artist from non-explicit-table

--- playlist_creator.py
+++ playlist_creator.py
@@ -1,32 +1,30 @@
 from spotify_auth import get_spotify_client
 import sqlite3
 
-# sp = get_spotify_client()
+sp = get_spotify_client()
 
-# user_id = sp.current_user()['id']
+user_id = sp.current_user()['id']
 
-# playlist = sp.user_playlist_create(
-#     user=user_id,
-#     name="Top Arctic Monkey Picks ❄️🐒",
-#     public=False,
-#     description="list of songs filtered for no explicit content."
-# )
+playlist = sp.user_playlist_create(
+    user=user_id,
+    name="Top Arctic Monkey Picks ❄️🐒",
+    public=False,
+    description="list of songs filtered for no explicit content."
+)
 
 
 def get_non_explicit_songs(artist: str):
     connection = sqlite3.connect('spotify_stats.db')
     cursor = connection.cursor()
+
     cursor.execute("""SELECT * FROM non_explicit_songs WHERE artist_name = ?""" , (artist,))
     
     results = cursor.fetchall()
-
-    for row in results:
-        print(row)
-
     connection.close()
+
+    return results
 
 
 def add_songs_to_playlist():
     pass
 
-get_non_explicit_songs(artist="Arctic Monkeys")
